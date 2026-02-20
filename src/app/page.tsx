ab140b27f10b47a4502e31123e2f609b06899931
blk182 <73johnparco@gmail.com>
2026-02-20 16:58:56 -0500

    fix: server-side redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-'use client'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-  
-  useEffect(() => {
-    router.push('/login')
-  }, [router])
-
-  return (
-    <div style={{ minHeight: '100vh', background: '#080808', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div style={{ color: '#C9A84C', fontSize: 24 }}>Cargando...</div>
-    </div>
-  )
+  redirect('/login')
 }
